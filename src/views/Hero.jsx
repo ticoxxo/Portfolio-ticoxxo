b88e9b60baf132233c679fff685d38fa2d83b162
ticoxxo <ticoxxo@gmail.com>
2019-07-11 17:22:54 -0600

    CSS

--- src/views/Hero.jsx
+++ src/views/Hero.jsx
@@ -32,7 +32,7 @@ const Hero = ({ children, offset }) => (
       <SVG icon="circle" hiddenMobile width={24} fill={colors['ticoxxo-magenta']} left="5%" top="70%" />
       <SVG icon="circle" width={6} fill={colors['ticoxxo-weak-blue']} left="4%" top="20%" /> 
       <SVG icon="upDown" width={8} fill={colors['ticoxxo-lilac']} left="95%" top="90%" />
-      <SVG icon="upDown" hiddenMobile width={24} fill={colors['grey-darker']} left="40%" top="80%" />
+      <SVG icon="upDown" hiddenMobile width={24} fill={colors['ticoxxo-strong-blue']} left="40%" top="80%" />
       <SVG icon="triangle" width={8} stroke={colors['ticoxxo-strong-blue']} left="25%" top="5%" />
       <SVG icon="circle" width={64} fill={colors['ticoxxo-lilac']} left="95%" top="5%" />
       <SVG icon="box" hiddenMobile width={64} fill={colors['ticoxxo-strong-blue']} left="5%" top="90%" />
